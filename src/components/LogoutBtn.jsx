e2b2ccab3197fa33f5e481e3980855ed2f5f8983
tanishs26 <tanishsharma2627@gmail.com>
2025-04-22 10:29:14 +0530

    logout problem

--- src/components/LogoutBtn.jsx
+++ src/components/LogoutBtn.jsx
@@ -3,12 +3,16 @@ import authService from "../appwrite/auth";
 import { logout } from "../store/authSlice";
 import { useDispatch } from "react-redux";
 import { Button } from "./import.js";
+import { useNavigate } from "react-router-dom";
+
 
 const LogoutBtn = () => {
+  const navigate=useNavigate()
   const dispatch = useDispatch();
   const logoutHandler = () => {
     authService.logout().then(() => {
       dispatch(logout());
+      navigate('/')
     });
   };
   return <button onClick={logoutHandler}   >
